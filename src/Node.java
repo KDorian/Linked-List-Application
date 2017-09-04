78ced3fe89552a389aaf71533d3d41ca0b315d31
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-04 17:49:44 +0200

    Add Node class

---
+++ src/Node.java
@@ -0,0 +1,39 @@
+
+public class Node<T> {
+
+    private T data;
+    Node<T> nextNode;
+
+    public Node(T data){
+
+        this.data = data;
+        nextNode = null;
+    }
+
+    public Node(T data, Node <T> next){
+
+        this.data = data;
+        nextNode = next;
+    }
+
+    public void setData(T data){
+
+        this.data = data;
+    }
+
+    public T getData(){
+
+        return data;
+    }
+
+    public void setNext(Node<T> next){
+
+        this.nextNode = next;
+    }
+
+    public Node <T> getText(){
+
+        return nextNode;
+    }
+
+}
